1234d02f9239cff3f96a99bbbc9116a837f46c7a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-01 14:23:55 -0300

    chore(server): create new route
    Get the number of registers on database:
    `GET /pools/count`

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,19 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query', 'warn', 'error'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
-    logger: true
+    logger: true,
+  })
+
+  fastify.get('/pools/count', async () => {
+    const pools = await prisma.pool.count()
+
+    return { count: pools }
   })
 
   await fastify.listen({ port: 3333 })
