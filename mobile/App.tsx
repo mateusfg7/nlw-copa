8ec3058157b4f0598f53aeda6c4b27ccc1529c07
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-03 11:05:48 -0300

    feat(mobile): use native-base statusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar'
-import { NativeBaseProvider, Center, Text } from 'native-base'
+import { NativeBaseProvider, StatusBar } from 'native-base'
 import {
   useFonts,
   Roboto_400Regular,
@@ -21,7 +20,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar style='light' />
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
       {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   )
