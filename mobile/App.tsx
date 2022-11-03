27e06b96e99f0890a44439f26e16b1fb414b4534
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-03 10:23:04 -0300

    feat(mobile): use native-base provider

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { NativeBaseProvider, Center, Text } from 'native-base'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider>
+      <StatusBar style='light' />
+      <Center flex={1} bgColor='gray.900'>
+        <Text color='gray.100' fontSize={24}>
+          Open up App.js to start working on your app!
+        </Text>
+      </Center>
+    </NativeBaseProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
