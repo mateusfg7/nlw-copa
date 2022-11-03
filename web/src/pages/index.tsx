c7c3f1b2f0acc0dcb296d03ddcad50bfac243ef5
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-03 15:52:34 -0300

    feat(web/ui): create styles

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,41 +7,58 @@ import iconCheckImg from '../assets/icon-check.svg'
 
 export default function Home() {
   return (
-    <div>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
         <Image src={logoImg} alt='NLW Copa' />
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
 
-        <div>
+        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className='mt-10 flex items-center gap-2'>
           <Image src={usersAvatarExampleImage} alt='' />
 
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-500'>+12.592</span> pessoas já estão
+            usando
           </strong>
         </div>
 
-        <form>
-          <input type='text' required placeholder='Qual o nome do seu bolão?' />
-          <button type='submit'>Criar meu bolão</button>
+        <form className='mt-10 flex gap-2'>
+          <input
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            type='text'
+            required
+            placeholder='Qual o nome do seu bolão?'
+          />
+          <button
+            className='bg-yellow-500 hover:bg-yellow-700 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase'
+            type='submit'
+          >
+            Criar meu bolão
+          </button>
         </form>
 
-        <p>
+        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
           Após criar seu bolão, você receberá um código único que poderá usar
           para convidar outras pessoas
         </p>
 
-        <div>
-          <div>
+        <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
+          <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt='' />
-            <div>
-              <span>+2.034</span>
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+2.034</span>
               <span>Bolões criados</span>
             </div>
           </div>
-          <div>
+
+          <div className='w-px h-14 bg-gray-600' />
+
+          <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt='' />
-            <div>
-              <span>+192.847</span>
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+192.847</span>
               <span>Palpites enviados</span>
             </div>
           </div>
