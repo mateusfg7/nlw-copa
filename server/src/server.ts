456d197fb88576fb99dd83d1008868a5750bfe29
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-03 12:46:42 -0300

    feat(server): new route `POST /pools`

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query', 'warn', 'error'],
@@ -19,6 +21,26 @@ async function bootstrap() {
     const pools = await prisma.pool.count()
 
     return { count: pools }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    })
+
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' })
