1fbf92061c8579ae208bd26f7ea00af0852032eb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-04 09:45:22 -0300

    feat(mobile/ux): create initial SignIn screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,24 @@
-import { Center, Text } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import { Button } from '../components/Button'
+
+import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor='gray.900'>
-      <Text color='white' fontSize={24} fontFamily='heading'>
-        Sign In
+    <Center flex={1} bgColor='gray.900' p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title='ENTRAR COM O GOOGLE'
+        type='SECONDARY'
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
+        mt={12}
+      />
+
+      <Text color='gray.300' textAlign='center' mt={4}>
+        Não utilizamos nenhuma informação além{'\n'}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   )
