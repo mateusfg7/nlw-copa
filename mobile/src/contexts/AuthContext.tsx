362af86963ca27a321444540ce861e31caa3a989
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-07 11:35:55 -0300

    feat(mobile/auth): connect AuthContext to api

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,8 @@ import * as AuthSession from 'expo-auth-session'
 import * as Google from 'expo-auth-session/providers/google'
 import * as WebBrowser from 'expo-web-browser'
 
+import { api } from '../services/api'
+
 WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
@@ -46,7 +48,24 @@ export function AuthContextProvider({ children }) {
   }
 
   async function signInWithGoogle(access_token: string) {
-    console.log('TOKEN DE AUTENTICAÇÃO | ', access_token)
+    try {
+      setIsUserLoading(true)
+
+      const tokenResponse = await api.post('/users', {
+        access_token,
+      })
+      api.defaults.headers.common[
+        'Authorization'
+      ] = `Bearer ${tokenResponse.data.token}`
+
+      const userInfoResponse = await api.get('/me')
+      setUser(userInfoResponse.data.user)
+    } catch (err) {
+      console.log(err)
+      throw err
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
