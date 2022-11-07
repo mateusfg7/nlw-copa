0225d74e41eb3b1f9123d2352d40b288d5282698
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-07 09:01:39 -0300

    feat(mobile/auth): use `useAuth` hook on SignIn screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,14 @@
 import { Center, Icon, Text } from 'native-base'
 import { Fontisto } from '@expo/vector-icons'
 
+import { useAuth } from '../hooks/useAuth'
 import { Button } from '../components/Button'
 
 import Logo from '../assets/logo.svg'
 
 export function SignIn() {
+  const { signIn, user } = useAuth()
+
   return (
     <Center flex={1} bgColor='gray.900' p={7}>
       <Logo width={212} height={40} />
@@ -14,6 +17,7 @@ export function SignIn() {
         type='SECONDARY'
         leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
         mt={12}
+        onPress={signIn}
       />
 
       <Text color='gray.300' textAlign='center' mt={4}>
